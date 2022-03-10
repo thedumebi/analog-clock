f351d7abcd5905d9abae1abf58b9fcc38d31adf8
Chiwuzoh Daniel <chiwuzohdumebi@gmail.com>
2022-03-10 15:28:50 +0100

    analaog clock

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Clock from "./components/Clock";
+import "./App.css";
 
-function App() {
+const App: React.FC = () => {
+  const [theme, setTheme] = useState("dark");
+
+  useEffect(() => {
+    const HTML: HTMLElement = document.querySelector("html")!;
+    HTML.setAttribute("data-theme", theme);
+
+    return () => {
+      HTML.setAttribute("data-theme", theme);
+    };
+  }, [theme]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app_container">
+      <Clock />
+
+      {theme === "light" ? (
+        <div className="theme">
+          <span onClick={() => setTheme("dark")}>Dark</span>
+        </div>
+      ) : (
+        <div className="theme">
+          <span onClick={() => setTheme("light")}>Light</span>
+        </div>
+      )}
+
+      <span>Made by DMB</span>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Clock/index.tsx
@@ -0,0 +1,97 @@
+import React, { useEffect, useRef, useState } from "react";
+
+import "./clock.css";
+
+interface Time {
+  hoursDeg: number;
+  minuteDeg: number;
+  secondsDeg: number;
+  numbers: number[];
+}
+
+interface Degrees {
+  transform?: string;
+}
+
+const useInterval = (callback: () => void, delay: number = 2000): void => {
+  const savedCallback: React.MutableRefObject<() => void> = useRef(() => {});
+
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  useEffect(() => {
+    const tick = () => {
+      savedCallback.current();
+    };
+    let id = setInterval(tick, delay);
+    return () => clearInterval(id);
+  }, [delay]);
+};
+
+const Clock: React.FC = () => {
+  const generateNumbers = (): number[] => {
+    return Array.from({ length: 12 }, (_, i) => i + 1);
+  };
+  const [time, setTime] = useState<Time>({
+    hoursDeg: 0,
+    minuteDeg: 0,
+    secondsDeg: 0,
+    numbers: generateNumbers(),
+  });
+
+  const pi = Math.PI;
+
+  const getTheTime = (): void => {
+    const currentDate: Date = new Date();
+    const secondsRatio: number = currentDate.getSeconds() / 60;
+    const minutesRatio: number = (secondsRatio + currentDate.getMinutes()) / 60;
+    const hoursRatio: number = (minutesRatio + currentDate.getHours()) / 12;
+
+    setTime((prevValues) => ({
+      ...prevValues,
+      hoursDeg: hoursRatio * 360,
+      minuteDeg: minutesRatio * 360,
+      secondsDeg: secondsRatio * 360,
+    }));
+  };
+
+  useInterval(getTheTime, 1000);
+
+  const setTheHands = (degrees: number): Degrees => {
+    return degrees
+      ? {
+          transform: `rotate(${degrees}deg)`,
+        }
+      : {};
+  };
+
+  const setClockNumbers = (n: number): Degrees => ({
+    transform: `translate(${Math.cos((pi * n) / 6 - pi / 2) * 135}px, ${
+      Math.sin((pi * n) / 6 - pi / 2) * 135
+    }px)`,
+  });
+
+  return (
+    <div className="container">
+      <div className="clock">
+        <div className="hours hand" style={setTheHands(time.hoursDeg)}></div>
+        <div className="minutes hand" style={setTheHands(time.minuteDeg)}></div>
+        <div
+          className="seconds hand"
+          style={setTheHands(time.secondsDeg)}
+        ></div>
+
+        {time.numbers.map((number) => (
+          <div className="numbers" key={number}>
+            <div className={`number${number}`} style={setClockNumbers(number)}>
+              {number}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Clock;
